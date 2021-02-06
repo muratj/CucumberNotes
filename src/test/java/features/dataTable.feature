9f221f288d69a19c8139a074cba7f7b608b81d74
murat <muratj.tm@gmail.com>
2021-02-06 16:31:56 -0500

    DataTable with Maps

--- src/test/java/features/dataTable.feature
+++ src/test/java/features/dataTable.feature
@@ -7,3 +7,12 @@ Feature: DataTable practice
       | Jenifer | Lopez     | 1990  | 9926426362 |
       | Elon    | Musk      | 1975  | 6425378482 |
     Then User registration should be successful
+
+  Scenario: DataTable Scenario2
+    Given Some registration page
+    When User enters following user details with columns
+      | firstname | lastname  | year  | phone      |
+      | Kevin     | McAlister | 1992  | 9927384732 |
+      | Jenifer   | Lopez     | 1990  | 9926426362 |
+      | Elon      | Musk      | 1975  | 6425378482 |
+    Then User registration should be successful
--- src/test/java/steps/DataTablePractice.java
+++ src/test/java/steps/DataTablePractice.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.util.List;
+import java.util.Map;
 
 public class DataTablePractice {
     @Given("Some registration page")
@@ -17,6 +18,21 @@ public class DataTablePractice {
     public void user_enters_following_user_details(DataTable data) {
         List<List<String>> dataList = data.asLists(String.class);
         dataList.forEach(System.out::println);
+    }
+
+    @When("User enters following user details with columns")
+    public void user_enters_following_user_details_with_columns(DataTable dataTable) {
+        List<Map<String, String>> users = dataTable.asMaps(String.class, String.class);
+
+//        users.forEach(System.out::println);
+
+        users.forEach(e -> {
+            System.out.print("|\t" + e.get("firstname"));
+            System.out.print("\t|\t" + e.get("lastname"));
+            System.out.print("\t|\t" + e.get("year"));
+            System.out.print("\t|\t" + e.get("phone") + "\t|" );
+            System.out.println();
+        });
     }
 
     @Then("User registration should be successful")

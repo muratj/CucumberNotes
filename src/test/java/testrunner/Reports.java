ca56b00d1722ab9e8ba5e53ab30ea163289f9a54
murat <muratj.tm@gmail.com>
2021-02-06 14:22:06 -0500

    Cucumber Reports: report.json, report.xml

---
+++ src/test/java/testrunner/Reports.java
@@ -0,0 +1,18 @@
+package testrunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features/example.feature"},
+        glue = {"steps"},
+        plugin = {"pretty",
+                "json:target/test-reports/report.json",
+                "junit:target/test-reports/report.xml"
+        }
+)
+
+public class Reports {
+}

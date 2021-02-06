baf626c5c1269118bc025251259d425d5342b2a7
murat <muratj.tm@gmail.com>
2021-02-06 17:50:44 -0500

    ScenarioOutline calculating examples

--- src/test/java/features/scenarioOutline.feature
+++ src/test/java/features/scenarioOutline.feature
@@ -1,15 +1,28 @@
-Feature: Login
+#Feature: Login
+#
+#  Scenario Outline: Login incorrect password
+#    Given Login page
+#    When User enters username "<UserName>"
+#    And User enters password "<Password>"
+#    And Clicks Login button
+#    Then Error message should be displayed
+#
+#    Examples:
+#      | UserName | Password |
+#      | user1    | 123456   |
+#      | user2    | qwerty   |
 
-  Scenario Outline: Login incorrect password
-    Given Login page
-    When User enters username "<UserName>"
-    And User enters password "<Password>"
-    And Clicks Login button
-    Then Error message should be displayed
+Feature: Calculator
 
+  Scenario Outline: Adding number
+    Given Calculator page
+    When user enters first number as <number1>
+    And user enters second number as <number2>
+    Then result should be <result>
 
 
     Examples:
-      | UserName | Password |
-      | user1    | 123456   |
-      | user2    | qwerty   |
+      | number1 | number2 | result |
+      |    2    |    2    |    4   |
+      |    5    |    6    |   11   |
+      |   100   |    80   |  180   |
--- src/test/java/steps/ScenarioOutlinePractice.java
+++ src/test/java/steps/ScenarioOutlinePractice.java
@@ -2,19 +2,22 @@ package steps;
 
 import io.cucumber.java.en.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class ScenarioOutlinePractice {
-
-
+    // Feature 1
     @Given("Login page")
     public void login_page() {
     }
 
     @When("User enters username {string}")
-    public void user_enters_username(String string) {
+    public void user_enters_username(String username) {
+        System.out.println("username = "+username);
     }
 
     @When("User enters password {string}")
-    public void user_enters_password(String string) {
+    public void user_enters_password(String password) {
+        System.out.println("password = " + password);
     }
 
     @When("Clicks Login button")
@@ -23,6 +26,33 @@ public class ScenarioOutlinePractice {
 
     @Then("Error message should be displayed")
     public void error_message_should_be_displayed() {
+    }
+
+    // Feature 2
+
+    int num1, num2, result;
+
+    @Given("Calculator page")
+    public void calculator_page() {
+    }
+
+    @When("user enters first number as {int}")
+    public void user_enters_first_number_as(Integer num1) {
+        this.num1 = num1;
+    }
+
+    @When("user enters second number as {int}")
+    public void user_enters_second_number_as(Integer num2) {
+        this.num2 = num2;
+    }
+
+    @Then("result should be {int}")
+    public void result_should_be(Integer result) {
+        this.result = num1 + num2;
+        System.out.println("expected " + result);
+        System.out.println("actual " + this.result);
+
+        assertEquals(this.result, (int) result);
     }
 
 }
